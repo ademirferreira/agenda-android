3d6eae158f55f87d0affe0d710348572d477b9ea
Ademir Ferreira <whoisademir@gmail.com>
2021-05-25 21:25:33 -0300

    Criando o menu de contexto via arquivo XML

--- app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.java
@@ -46,7 +46,7 @@ public class ListaAlunosActivity extends AppCompatActivity {
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
-        menu.add("Remover");
+        getMenuInflater().inflate(R.menu.activity_lista_alunos_menu, menu);
     }
 
     @Override
