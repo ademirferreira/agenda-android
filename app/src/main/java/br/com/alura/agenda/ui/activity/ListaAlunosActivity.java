b790241ea5bdafa476e8bcc122270e9cd7b5b35b
Ademir Ferreira <whoisademir@gmail.com>
2021-05-26 21:07:24 -0300

    Implementando Adapter personalizado

--- app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.java
@@ -25,6 +25,7 @@ import java.util.List;
 import br.com.alura.agenda.R;
 import br.com.alura.agenda.dao.AlunoDAO;
 import br.com.alura.agenda.model.Aluno;
+import br.com.alura.agenda.ui.adapter.ListaAlunosAdapter;
 
 import static br.com.alura.agenda.ui.activity.ConstantesActivities.CHAVE_ALUNO;
 
@@ -32,7 +33,8 @@ public class ListaAlunosActivity extends AppCompatActivity {
 
     public static final String TITULO_APPBAR = "Lista de alunos";
     private final AlunoDAO dao = new AlunoDAO();
-    private ArrayAdapter<Aluno> adapter;
+    private ListaAlunosAdapter adapter;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -114,31 +116,8 @@ public class ListaAlunosActivity extends AppCompatActivity {
     }
 
     private void configuraAdapter(ListView listaDeAlunos) {
-        adapter = new ArrayAdapter<>(this, R.layout.item_aluno);
-        listaDeAlunos.setAdapter(new BaseAdapter() {
-            
-            private final List<Aluno> alunos = new ArrayList<>();
-            @Override
-            public int getCount() {
-                return alunos.size();
-            }
 
-            @Override
-            public Aluno getItem(int position) {
-                return alunos.get(position);
-            }
-
-            @Override
-            public long getItemId(int position) {
-                return alunos.get(position).getId();
-            }
-
-            @Override
-            public View getView(int position, View convertView, ViewGroup viewGroup) {
-                View viewCriada = LayoutInflater.from(ListaAlunosActivity.this)
-                        .inflate(R.layout.item_aluno, viewGroup);
-                return viewCriada;
-            }
-        });
+        adapter = new ListaAlunosAdapter(this);
+        listaDeAlunos.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/br/com/alura/agenda/ui/adapter/ListaAlunosAdapter.java
@@ -0,0 +1,57 @@
+package br.com.alura.agenda.ui.adapter;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.alura.agenda.R;
+import br.com.alura.agenda.model.Aluno;
+
+public class ListaAlunosAdapter extends BaseAdapter {
+    private final List<Aluno> alunos = new ArrayList<>();
+    private Context context;
+
+    public ListaAlunosAdapter(Context context){
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return alunos.size();
+    }
+
+    @Override
+    public Aluno getItem(int position) {
+        return alunos.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return alunos.get(position).getId();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup viewGroup) {
+        @SuppressLint("ViewHolder") View viewCriada = LayoutInflater.from(context)
+                .inflate(R.layout.item_aluno, viewGroup, false);
+        return viewCriada;
+    }
+
+    public void clear() {
+        alunos.clear();
+    }
+
+    public void addAll(List<Aluno> alunos) {
+        this.alunos.addAll(alunos);
+    }
+
+    public void remove(Aluno aluno) {
+        alunos.remove(aluno);
+    }
+}
