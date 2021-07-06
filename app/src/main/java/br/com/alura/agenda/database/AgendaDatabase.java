a6b569c77ce54a859136e73a4e5d6de0150626a1
Ademir Ferreira <whoisademir@gmail.com>
2021-07-06 20:01:26 -0300

    Implementando componentes principais do Room

---
+++ app/src/main/java/br/com/alura/agenda/database/AgendaDatabase.java
@@ -0,0 +1,10 @@
+package br.com.alura.agenda.database;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+import br.com.alura.agenda.model.Aluno;
+
+@Database(entities = { Aluno.class }, version = 1, exportSchema = false)
+public abstract class AgendaDatabase extends RoomDatabase {
+}
---
+++ app/src/main/java/br/com/alura/agenda/database/dao/RoomAlunoDAO.java
@@ -0,0 +1,7 @@
+package br.com.alura.agenda.database.dao;
+
+import androidx.room.Dao;
+
+@Dao
+public interface RoomAlunoDAO {
+}
